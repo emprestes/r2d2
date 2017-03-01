9b97a2ba4c17c28e98cfd091dc537c0a1e6a5d33
Prestes, E. M <emprestes@gmail.com>
2017-03-01 02:06:46 -0300

    #3 Implementar models (Species) para teste unitários

--- core/src/main/java/tradeforce/starwars/domain/model/Species.java
+++ core/src/main/java/tradeforce/starwars/domain/model/Species.java
@@ -43,112 +43,126 @@ public class Species implements Domain<Species>, Serializable {
         return name;
     }
 
-    public void setName(String name) {
+    public Species setName(String name) {
         this.name = name;
+        return this;
     }
 
     public String getClassification() {
         return classification;
     }
 
-    public void setClassification(String classification) {
+    public Species setClassification(String classification) {
         this.classification = classification;
+        return this;
     }
 
     public String getDesignation() {
         return designation;
     }
 
-    public void setDesignation(String designation) {
+    public Species setDesignation(String designation) {
         this.designation = designation;
+        return this;
     }
 
     public Integer getAverage_height() {
         return average_height;
     }
 
-    public void setAverage_height(Integer average_height) {
+    public Species setAverage_height(Integer average_height) {
         this.average_height = average_height;
+        return this;
     }
 
     public String getSkin_colors() {
         return skin_colors;
     }
 
-    public void setSkin_colors(String skin_colors) {
+    public Species setSkin_colors(String skin_colors) {
         this.skin_colors = skin_colors;
+        return this;
     }
 
     public String getHair_colors() {
         return hair_colors;
     }
 
-    public void setHair_colors(String hair_colors) {
+    public Species setHair_colors(String hair_colors) {
         this.hair_colors = hair_colors;
+        return this;
     }
 
     public String getEye_colors() {
         return eye_colors;
     }
 
-    public void setEye_colors(String eye_colors) {
+    public Species setEye_colors(String eye_colors) {
         this.eye_colors = eye_colors;
+        return this;
     }
 
     public Integer getAverage_lifespan() {
         return average_lifespan;
     }
 
-    public void setAverage_lifespan(Integer average_lifespan) {
+    public Species setAverage_lifespan(Integer average_lifespan) {
         this.average_lifespan = average_lifespan;
+        return this;
     }
 
     public String getHomeworld() {
         return homeworld;
     }
 
-    public void setHomeworld(String homeworld) {
+    public Species setHomeworld(String homeworld) {
         this.homeworld = homeworld;
+        return this;
     }
 
     public String getLanguage() {
         return language;
     }
 
-    public void setLanguage(String language) {
+    public Species setLanguage(String language) {
         this.language = language;
+        return this;
     }
 
     public List<Person> getPeople() {
         return people;
     }
 
-    public void setPeople(List<Person> people) {
+    public Species setPeople(List<Person> people) {
         this.people = people;
+        return this;
     }
 
     public List<Film> getFilms() {
         return films;
     }
 
-    public void setFilms(List<Film> films) {
+    public Species setFilms(List<Film> films) {
         this.films = films;
+        return this;
     }
 
     public String getCreated() {
         return created;
     }
 
-    public void setCreated(String created) {
+    public Species setCreated(String created) {
         this.created = created;
+        return this;
     }
 
     public String getEdited() {
         return edited;
     }
 
-    public void setEdited(String edited) {
+    public Species setEdited(String edited) {
         this.edited = edited;
+        return this;
     }
 
     @Override
--- core/src/test/java/tradeforce/starwars/domain/model/SpeciesSerializabilityTest.java
+++ core/src/test/java/tradeforce/starwars/domain/model/SpeciesSerializabilityTest.java
@@ -1,5 +1,30 @@
 package tradeforce.starwars.domain.model;
 
-public class SpeciesSerializabilityTest {
+import java.io.Serializable;
 
+import junitx.extensions.SerializabilityTestCase;
+
+public class SpeciesSerializabilityTest extends SerializabilityTestCase {
+
+    public SpeciesSerializabilityTest(String name) {
+        super(name);
+    }
+
+    @Override
+    protected Serializable createInstance() throws Exception {
+        return new Species()
+                .setName("species1")
+                .setAverage_height(1)
+                .setAverage_lifespan(1)
+                .setClassification("species1")
+                .setCreated("species1")
+                .setDesignation("species1")
+                .setEdited("species1")
+                .setEye_colors("species1")
+                .setHair_colors("species1")
+                .setHomeworld("species1")
+                .setLanguage("species1")
+                .setSkin_colors("species1")
+                .setUrl("species1");
+    }
 }
