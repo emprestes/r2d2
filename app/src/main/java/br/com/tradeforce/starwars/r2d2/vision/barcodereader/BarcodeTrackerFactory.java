aa4b8044eea3f3c5ff720841968fd6c825e292e7
Prestes, E. M <emprestes@gmail.com>
2017-03-01 04:04:09 -0300

    #3 Capturar QR-Code de um site (Barcode Vision)

---
+++ app/src/main/java/br/com/tradeforce/starwars/r2d2/vision/barcodereader/BarcodeTrackerFactory.java
@@ -0,0 +1,27 @@
+package br.com.tradeforce.starwars.r2d2.vision.barcodereader;
+
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import br.com.tradeforce.starwars.r2d2.vision.ui.camera.BarcodeGraphic;
+import br.com.tradeforce.starwars.r2d2.vision.ui.camera.GraphicOverlay;
+
+/**
+ * Factory for creating a tracker and associated graphic to be associated with a new barcode.  The
+ * multi-processor uses this factory to create barcode trackers as needed -- one for each barcode.
+ */
+class BarcodeTrackerFactory implements MultiProcessor.Factory<Barcode> {
+    private GraphicOverlay<BarcodeGraphic> mGraphicOverlay;
+
+    BarcodeTrackerFactory(GraphicOverlay<BarcodeGraphic> barcodeGraphicOverlay) {
+        mGraphicOverlay = barcodeGraphicOverlay;
+    }
+
+    @Override
+    public Tracker<Barcode> create(Barcode barcode) {
+        BarcodeGraphic graphic = new BarcodeGraphic(mGraphicOverlay);
+        return new BarcodeGraphicTracker(mGraphicOverlay, graphic);
+    }
+
+}
