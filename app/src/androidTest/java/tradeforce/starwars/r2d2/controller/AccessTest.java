7e221d92e107b9c20855dc9ab46d4e0dd8ca049c
Prestes, E. M <emprestes@gmail.com>
2017-03-05 02:31:08 -0300

    #8 Entrada e saída do app

---
+++ app/src/androidTest/java/tradeforce/starwars/r2d2/controller/AccessTest.java
@@ -0,0 +1,54 @@
+package tradeforce.starwars.r2d2.controller;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.pressBack;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AccessTest {
+
+    @Rule
+    public ActivityTestRule<SplashActivity_> mActivityTestRule = new ActivityTestRule<>(SplashActivity_.class);
+
+    @Test
+    public void accessTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(400);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        pressBack();
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(2950);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        pressBack();
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
